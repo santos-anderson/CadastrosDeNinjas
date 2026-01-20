a08d5f734a0695d4a5fd661b3228e1824861491d
Anderson Alves Santos <santos.anders@gmail.com>
2026-01-20 11:18:51 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastrosDeNinjas/CadastrosDeNinjasApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastrosDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosDeNinjasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosDeNinjasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/java10x/CadastrosDeNinjas/CadastrosDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastrosDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
