0a900d74e76f9692637553107acc968999362bf0
Anderson Alves Santos <santos.anders@gmail.com>
2026-01-21 11:17:11 -0300

    feat: Coloca o lombok na classe para abstração

--- src/main/java/dev/java10x/CadastrosDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,19 @@ package dev.java10x.CadastrosDeNinjas.Missoes;
 
 import dev.java10x.CadastrosDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class MissoesModel {
 
     @Id
@@ -18,38 +26,4 @@ public class MissoesModel {
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
 
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nome, String dificuldade, List<NinjaModel> ninjas) {
-        this.nome = nome;
-        Dificuldade = dificuldade;
-        this.ninjas = ninjas;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-
-    public String getDificuldade() {
-        return Dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        Dificuldade = dificuldade;
-    }
-
-    public List<NinjaModel> getNinjas() {
-        return ninjas;
-    }
-
-    public void setNinjas(List<NinjaModel> ninjas) {
-        this.ninjas = ninjas;
-    }
 }
--- src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaModel.java
@@ -4,13 +4,18 @@ package dev.java10x.CadastrosDeNinjas.Ninjas;
 
 import dev.java10x.CadastrosDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //Entity ele transforma a classe em uma tabela no banco de dados
 //JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class NinjaModel {
 
     @Id
@@ -25,44 +30,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") //nome da coluna que vai receber a chave estrangeira
     private MissoesModel missoes;
 
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(Long id, String nome, String email, int idade) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-
-
-    public String getNome() {
-
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-
-
-    }
 }
