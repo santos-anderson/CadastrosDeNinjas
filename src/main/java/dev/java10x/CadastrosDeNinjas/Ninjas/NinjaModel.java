9e6e2a8593e43e7bba2a006106dc1ac7a728e283
Anderson Alves Santos <santos.anders@gmail.com>
2026-01-23 19:00:46 -0300

    refactor: lombook tostring adicionado

--- src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastrosDeNinjas/Ninjas/NinjaModel.java
@@ -1,12 +1,11 @@
 package dev.java10x.CadastrosDeNinjas.Ninjas;
 
-
-
 import dev.java10x.CadastrosDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 //Entity ele transforma a classe em uma tabela no banco de dados
 //JPA = Java Persistence API
@@ -15,6 +14,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = "missoes")
 
 public class NinjaModel {
 
